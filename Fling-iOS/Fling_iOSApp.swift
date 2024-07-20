72a73406f46e88c73692b1ef37825278434cda88
Larson Carter <larsoncarter72404@gmail.com>
2024-07-20 03:09:25 -0500

    Initial Commit

---
+++ Fling-iOS/Fling_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fling_iOSApp.swift
+//  Fling-iOS
+//
+//  Created by Larson Carter on 7/20/24.
+//
+
+import SwiftUI
+
+@main
+struct Fling_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
